ec668bb6290bf75f5fa816bc9d7c854efb88d38e
xibenn <Spelenleren@1302>
2018-10-30 15:23:23 +0100

    DEV-006: Made the "Cardview" prettier.

--- src/nsreisplanner/urls.py
+++ src/nsreisplanner/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='homepage'),
     url(r'^reisplanner/', views.reisplanner, name='reisplanner'),
+    url(r'^details/', views.details, name='details'),
 ]
